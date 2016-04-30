c46b82cf95d8574c91fe0e8b7b5759e9f543f883
Anton Podviaznikov <podviaznikov@gmail.com>
2016-04-29 17:46:07 -0700

    better unit tests

--- test/docker.js
+++ test/docker.js
@@ -8,6 +8,35 @@ const Docker = require('../index').Docker
 
 describe('Docker', function () {
   describe('constructor', () => {
+    /*eslint-disable no-new */
+    it('should thow if options are empty', (done) => {
+      assert.throws(() => {
+        new Docker()
+      }, Error, 'Options are not defined')
+      done()
+    })
+
+    it('should thow if host is not defined', (done) => {
+      assert.throws(() => {
+        new Docker({})
+      }, Error, 'Docker host is not defined')
+      done()
+    })
+
+    it('should thow if serviceNae is not defined', (done) => {
+      assert.throws(() => {
+        new Docker({ host: 'https://10.0.0.1:4242' })
+      }, Error, 'Service name is not defined')
+      done()
+    })
+
+    it('should thow if host is invalid', (done) => {
+      assert.throws(() => {
+        new Docker({ host: 213123, serviceName: 'loki' })
+      }, Error, 'Parameter \'url\' must be a string, not number')
+      done()
+    })
+
     it('should setup docker', (done) => {
       let docker
       assert.doesNotThrow(() => {
@@ -16,5 +45,6 @@ describe('Docker', function () {
       assert.instanceOf(docker.client, Dockerode)
       done()
     })
+    /*eslint-disable no-new */
   })
 })
--- test/swarm.js
+++ test/swarm.js
@@ -8,6 +8,35 @@ const Swarm = require('../index').Swarm
 
 describe('Swarm', function () {
   describe('constructor', () => {
+    /*eslint-disable no-new */
+    it('should thow if options are empty', (done) => {
+      assert.throws(() => {
+        new Swarm()
+      }, Error, 'Options are not defined')
+      done()
+    })
+
+    it('should thow if host is not defined', (done) => {
+      assert.throws(() => {
+        new Swarm({})
+      }, Error, 'Docker host is not defined')
+      done()
+    })
+
+    it('should thow if serviceNae is not defined', (done) => {
+      assert.throws(() => {
+        new Swarm({ host: 'https://10.0.0.1:4242' })
+      }, Error, 'Service name is not defined')
+      done()
+    })
+
+    it('should thow if host is invalid', (done) => {
+      assert.throws(() => {
+        new Swarm({ host: 213123, serviceName: 'loki' })
+      }, Error, 'Parameter \'url\' must be a string, not number')
+      done()
+    })
+
     it('should setup docker', (done) => {
       let swarm
       assert.doesNotThrow(() => {
@@ -16,5 +45,6 @@ describe('Swarm', function () {
       assert.instanceOf(swarm.client, Dockerode)
       done()
     })
+    /*eslint-disable no-new */
   })
 })
