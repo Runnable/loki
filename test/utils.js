ee0724a9b99e3493bef5cd4417df355295dc14d1
Anton Podviaznikov <podviaznikov@gmail.com>
2016-10-18 13:32:55 -0700

    add new utils file and tests

---
+++ test/utils.js
@@ -0,0 +1,34 @@
+'use strict'
+
+require('loadenv')()
+
+const chai = require('chai')
+const assert = chai.assert
+
+const utils = require('../../lib/utils')
+
+describe('utils unit test', () => {
+  describe('toDockerHost', () => {
+    it('should convert url to host', (done) => {
+      assert.equal(utils.toDockerHost('http://10.0.0.1:4242'), '10.0.0.1:4242')
+      done()
+    })
+
+    it('should return same valid host', (done) => {
+      assert.equal(utils.toDockerHost('10.0.0.1:4242'), '10.0.0.1:4242')
+      done()
+    })
+  })
+
+  describe('toDockerUrl', () => {
+    it('should convert host to url', (done) => {
+      assert.equal(utils.toDockerUrl('10.0.0.1:4242'), 'http://10.0.0.1:4242')
+      done()
+    })
+
+    it('should return same valid url', (done) => {
+      assert.equal(utils.toDockerUrl('http://10.0.0.1:4242'), 'http://10.0.0.1:4242')
+      done()
+    })
+  })
+})
